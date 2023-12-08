a42dbf58a4102067fa09534f5357ad94713f91bd
oulam <oumarlamsn@gmail.com>
2023-12-08 18:28:49 +0000

    Add navbar to sidebar

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,3 +1,7 @@
+// ******TO DO******
+// - Replace logo and sub-logo by mines 
+// - Change colors (optional)
+
 import { Link, NavLink } from "react-router-dom";
 import "./index.scss";
 import LogoS from "../../assets/images/logo-s.png";
@@ -7,37 +11,36 @@ import { faEnvelope, faHome, faUser } from "@fortawesome/free-solid-svg-icons";
 
 const Sidebar = () => {
   return (
-    <>
       <div className="nav-bar">
         <Link className="logo" to="/">
           <img src={LogoS} alt="logo" />
-          <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
+          <img className="sub-logo" src={LogoSubtitle} alt="lamdev" />
         </Link>
+
+        <nav>
+          <NavLink exact="true" activeclassname="active" to="/">
+            <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+          </NavLink>
+
+          <NavLink
+            exact="true"
+            activeclassname="active"
+            className="about-link"
+            to="/about"
+          >
+            <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+          </NavLink>
+
+          <NavLink
+            exact="true"
+            activeclassname="active"
+            className="contact-link"
+            to="/contact"
+          >
+            <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+          </NavLink>
+        </nav>
       </div>
-      <nav>
-        <NavLink exact="true" activeclassname="active" to="/">
-          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
-        </NavLink>
-
-        <NavLink
-          exact="true"
-          activeclassname="active"
-          className="about-link"
-          to="/about"
-        >
-          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
-        </NavLink>
-
-        <NavLink
-          exact="true"
-          activeclassname="active"
-          className="contact-link"
-          to="/contact"
-        >
-          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
-        </NavLink>
-      </nav>
-    </>
   );
 };
 
