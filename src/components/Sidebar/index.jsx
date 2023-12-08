5ddb4b3c9f0edc7f707751823b5225263e7070e4
oulam <oumarlamsn@gmail.com>
2023-12-08 19:42:28 +0000

    Add social media links to navbar

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,5 +1,5 @@
 // ******TO DO******
-// - Replace logo and sub-logo by mines 
+// - Replace logo and sub-logo by mines
 // - Change colors (optional)
 
 import { Link, NavLink } from "react-router-dom";
@@ -8,39 +8,60 @@ import LogoS from "../../assets/images/logo-s.png";
 import LogoSubtitle from "../../assets/images/logo_sub.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope, faHome, faUser } from "@fortawesome/free-solid-svg-icons";
+import {faGithub, faLinkedin, faXTwitter }from "@fortawesome/free-brands-svg-icons";
 
 const Sidebar = () => {
   return (
-      <div className="nav-bar">
-        <Link className="logo" to="/">
-          <img src={LogoS} alt="logo" />
-          <img className="sub-logo" src={LogoSubtitle} alt="lamdev" />
-        </Link>
+    <div className="nav-bar">
+      <Link className="logo" to="/">
+        <img src={LogoS} alt="logo" />
+        <img className="sub-logo" src={LogoSubtitle} alt="lamdev" />
+      </Link>
 
-        <nav>
-          <NavLink exact="true" activeclassname="active" to="/">
-            <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
-          </NavLink>
+      <nav>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
 
-          <NavLink
-            exact="true"
-            activeclassname="active"
-            className="about-link"
-            to="/about"
-          >
-            <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
-          </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="about-link"
+          to="/about"
+        >
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
 
-          <NavLink
-            exact="true"
-            activeclassname="active"
-            className="contact-link"
-            to="/contact"
-          >
-            <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
-          </NavLink>
-        </nav>
-      </div>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+      </nav>
+
+      <ul>
+        <li>
+          <a target="_blank" rel="noreferrer" href="https://github.com/OumarLAM/Oumar_LAM">
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
+        </li>
+
+        <li>
+          <a target="_blank" rel="noreferrer" href="https://twitter.com/oumarlam_fcb">
+            <FontAwesomeIcon icon={faXTwitter} color="#4d4d4e" />
+          </a>
+        </li>
+
+        <li>
+          <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/oumar-lam-692b641a7/">
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
+        </li>
+      </ul>
+    </div>
   );
 };
 
