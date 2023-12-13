56f96bd1393cba5a0435b5e55a96954a800c3ba5
oulam <oumarlamsn@gmail.com>
2023-12-13 18:19:11 +0000

    Add loader page

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -3,6 +3,7 @@ import LogoTitle2 from "../../assets/images/logo-s2.png";
 import "./index.scss";
 import AnimatedLetters from "../AnimatedLetters";
 import { useEffect, useState } from "react";
+import Loader from 'react-loaders';
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState("text-animate");
@@ -33,33 +34,36 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="container home-page">
-      <div className="text-zone">
-        <h1>
-          <span className={letterClass}>H</span>
-          <span className={`${letterClass} _12`}>i,</span>
-          <br />
-          <span className={`${letterClass} _13`}>I</span>
-          <span className={`${letterClass} _14`}>&apos;m</span>
-          <img src={LogoTitle2} alt="Developer" />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={name}
-            index={15}
-          />
-          <br />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={job}
-            index={20}
-          />
-        </h1>
-        <h2>Problem Solver | Eternal Learner | Full Stack Developer </h2>
-        <Link to="/contact" className="flat-button">
-          CONTACT ME
-        </Link>
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>&apos;m</span>
+            <img src={LogoTitle2} alt="Developer" />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={name}
+              index={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={job}
+              index={20}
+            />
+          </h1>
+          <h2>Problem Solver | Eternal Learner | Full Stack Developer </h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
       </div>
-    </div>
+      <Loader type="ball-beat" />
+    </>
   );
 };
 
